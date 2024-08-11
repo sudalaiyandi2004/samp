792b93526398533027c63f910ef723f4c495944a
sudalaiyandi sabarish kumar <sudalaiyandi@student.tce.edu>
2024-08-11 11:40:41 +0530

    Your descriptive message about the changes

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(express.json());
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  tlsAllowInvalidCertificates: true,
 });
 
 const itemSchema = new mongoose.Schema({
